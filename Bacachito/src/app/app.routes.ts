56e1696550376b29451148bc5f1f52441ea3677b
Angul0 <anguloalan55@gmail.com>
2026-02-08 13:11:38 -0700

    componente de inicio creado y sujeto a cambio de diseno

--- Bacachito/src/app/app.routes.ts
+++ Bacachito/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { provideRouter } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { Gestion } from './gestion/gestion';
 
 export const routes: Routes = [
     {path: '', redirectTo: '/login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
     {path: 'register', component: RegisterComponent},
+    { path: 'gestion', component: Gestion }
 ];
 
 export const appRouter = provideRouter(routes);
