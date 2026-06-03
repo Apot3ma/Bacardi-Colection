c8b308b6d22f43b218b34c60fdc6b854c4e1b30f
Fili <filiberogalvez@gmail.com>
2026-06-03 17:54:04 -0600

    hot fix para USER LOG

---
+++ Bacachito/Backend/CrudsNew/User_logs.js
@@ -0,0 +1,71 @@
+import { Router } from "express";
+import { stat } from "node:fs";
+
+const router = Router();
+
+// id 'autoincrementado'
+// id_project
+// id_user
+// role
+
+
+// =====================
+// ==CREAR UN USER LOG==
+// =====================
+        // SE SOLICITA ID DEL PROYECTO,ID DEL USUARIIO , ROL DEL USUARIO
+router.post('/' , async (req,res) =>{
+    const pool =req.app.locals.pool;
+    const {id_project,id_user,role} = req.body;
+    try{
+        const [result] = await pool.query(
+            'INSERT INTO user_log_project (id_project,id_user,role) VALUES (?,?,?)',
+            [id_project,id_user,role]
+        );
+            res.status(200).json({id:result.insertId})
+    }catch(err){
+        res.status(500).json({error: 'Error', details:err.message})
+    }
+});
+
+// ===========================================
+// ==VER TODAS LAS CATEGORIAS DE UN PROYECTO==
+// ===========================================
+        // SE SOLICITA ID DEL PROYECTO
+router.get('/:id_project', async (req,res)=>{
+    const pool =req.app.locals.pool;
+     const {id_project} = req.params;
+    try{
+        const [result] = await pool.query(
+            'SELECT * FROM user_log_project WHERE id_project = ? VALUES (?)',
+            [id_project]
+        );
+         if (result.length === 0) {
+            return res.status(404).json({ error: 'no existe el proyecto' });
+        }
+        res.json(result);       
+    }catch(err){
+        res.status(500).json({error: 'Error', details:err.message}
+        )
+    }
+});
+
+// ===========================================
+// ==VER TODOS LOS USUARIOS DE UNA CATEGORIA==
+// ===========================================
+router.get('/:role', async (req,res)=>{
+    const pool =req.app.locals.pool;
+     const {role} = req.params;
+    try{
+        const [result] = await pool.query(
+            'SELECT * FROM user_log_project WHERE role = ? VALUES (?)',
+            [role]
+        );
+         if (result.length === 0) {
+            return res.status(404).json({ error: 'no existe el rol' });
+        }
+        res.json(result);       
+    }catch(err){
+        res.status(500).json({error: 'Error', details:err.message}
+        )
+    }
+});
